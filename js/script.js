325df8aa2b879e95a53d9ad1bb1093e452a8da66
Gabisita <gabilopezdiaz22@gmail.com>
2023-07-12 18:24:43 -0500

    actualizado

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ const modal = document.getElementById('ContainerModal');
 
 const openButton = document.getElementById('OpenFirstModal');
 openButton.onclick = open;
+modal.style.display = 'none';
 
 function open(){
-  modal.style.display = 'block';
+  document.startViewTransition(()=> modal.style.display = 'block')
 }
 
 const closeButton = document.getElementById('CloseButton');
 closeButton.onclick = close;
 
 function close(){
-  modal.style.display = 'none';
+  document.startViewTransition(()=> modal.style.display = 'none')
 }
